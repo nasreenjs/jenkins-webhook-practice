ed23f7b34619ad460aa6afaae97106b6d3d77a6b
Nasreen <nasreen201038@gmail.com>
2024-10-02 17:06:12 +0530

    sp boot

---
+++ src/main/java/com/example/jenkins_webhook_practice/JenkinsWebhookPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_webhook_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebhookPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWebhookPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkins_webhook_practice/JenkinsWebhookPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_webhook_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWebhookPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
